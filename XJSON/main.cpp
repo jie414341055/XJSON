8cf17b401c2882e8373e8a51d49eee093bf9005b
jie414341055 <jie414341055@gmail.com>
2015-03-26 14:49:24 +0800

    Initial Commit

---
+++ XJSON/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XJSON
+//
+//  Created by minjie on 3/26/15.
+//  Copyright (c) 2015 minjie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
