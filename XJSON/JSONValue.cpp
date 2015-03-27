6fbb13e47e8743d4b21cbf9eef2dfc6c4475e34b
jie414341055 <jie414341055@gmail.com>
2015-03-27 13:19:48 +0800

    start jsonvalue

--- XJSON/JSONValue.cpp
+++ XJSON/JSONValue.cpp
@@ -7,3 +7,39 @@
 //
 
 #include "JSONValue.h"
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+
+
+JSONValue *JSONValue::Parse(const wchar_t **data) {
+    // string
+    if (**data == '"') {
+        std::wstring str;
+        if (!JSON::ExtractString(&(++(*data)), str)) {
+            return NULL;
+        } else {
+            return new JSONValue(str);
+        }
+    // boolean
+    } else if ((hasLength(*data, 4) && wcsncasecmp(*data, L"true", 4) == 0) || (hasLength(*data, 5) && wcsncasecmp(*data, L"false", 5) == 0)) {
+        bool value = wcsncasecmp(*data, L"true", 4) == 0;
+        (*data) += value ? 4 : 5;
+        return new JSONValue(value);
+    // null
+    } else if (hasLength(*data, 4) && wcsncasecmp(*data, L"null", 4) == 0) {
+        (*data) += 4;
+        return new JSONValue();
+    // number
+    } else if (**data == L'-' || (**data >= L'0' && **data <= L'9')) {
+        // negative
+        bool neg = **data == L'-';
+        
+    }
+}
